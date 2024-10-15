209b505e80c110c333a9f98dde0539bb8789a5ca
David Cuadrado <dav.cuadrado@gmail.com>
2024-10-15 16:53:28 +0200

    structure update

--- S05T01N01/src/main/java/cat/itacademy/s05/t01/n01/model/Game.java
+++ S05T01N01/src/main/java/cat/itacademy/s05/t01/n01/model/Game.java
@@ -1,5 +1,7 @@
 package cat.itacademy.s05.t01.n01.model;
 
 public class Game {
+	
+	private int number;
 
 }
--- S05T01N01/src/main/java/cat/itacademy/s05/t01/n01/repositories/PlayerRepository.java
+++ S05T01N01/src/main/java/cat/itacademy/s05/t01/n01/repositories/PlayerRepository.java
@@ -1,8 +1,10 @@
 package cat.itacademy.s05.t01.n01.repositories;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import cat.itacademy.s05.t01.n01.model.Player;
 
 @Repository
-public class PlayerRepository extends JpaRepository<Player, Integer> {
+public interface PlayerRepository extends JpaRepository<Player, Integer> {
 
 }
