01cc46c7a55ced1810de0bfb9dd5362c5b7e27f4
David Cuadrado <dav.cuadrado@gmail.com>
2024-10-17 11:42:52 +0200

    GameController and GameService structure update

--- S05T01N01/src/main/java/cat/itacademy/s05/t01/n01/controllers/GameController.java
+++ S05T01N01/src/main/java/cat/itacademy/s05/t01/n01/controllers/GameController.java
@@ -3,6 +3,7 @@ package cat.itacademy.s05.t01.n01.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,9 +23,6 @@ public class GameController {
 	@Autowired
 	private GameService gameService;
 
-	public GameController(GameService gameService) {
-		this.gameService = gameService;
-	}
 
 	@PostMapping("/new")
 	public Mono<ResponseEntity<Game>> createNewGame(@RequestBody Mono<Player> playerMono) {
@@ -35,8 +33,23 @@ public class GameController {
 	}
 
 	@GetMapping("/{id}")
-	public Mono<ResponseEntity<Game>> getGameDetails(@PathVariable String id) {
-		return gameService.getGameById(id).map(game -> ResponseEntity.status(HttpStatus.OK).body(game));
+	public Mono<ResponseEntity<Game>> getGameDetails(@PathVariable String gameId) {
+		return gameService.getGameById(gameId).map(game -> ResponseEntity.status(HttpStatus.OK).body(game));
+	}
+
+	@PostMapping("/{id}/play")
+	public Mono<ResponseEntity<Game>> makePlay(@PathVariable String gameId, String playType, int bid) {
+		// PLACERHOLDER
+		return gameService.nextPlayType(gameId, playType, bid)
+				.map(game -> ResponseEntity.status(HttpStatus.OK).body(game));
+		// desarrollar
+	}
+
+	@DeleteMapping
+	public Mono<ResponseEntity<String>> deleteGame(@PathVariable String gameId) {
+		return gameService.deleteGameById(gameId).map(
+				game -> ResponseEntity.status(HttpStatus.NO_CONTENT).body("Game " + gameId + " deleted succesfully"));
+
 	}
 
 }
--- S05T01N01/src/main/java/cat/itacademy/s05/t01/n01/services/GameService.java
+++ S05T01N01/src/main/java/cat/itacademy/s05/t01/n01/services/GameService.java
@@ -1,6 +1,8 @@
 package cat.itacademy.s05.t01.n01.services;
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import cat.itacademy.s05.t01.n01.model.Game;
 import cat.itacademy.s05.t01.n01.model.Player;
 import cat.itacademy.s05.t01.n01.repositories.GameRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -34,6 +37,25 @@ public class GameService {
 	public Mono<Game> getGameById(String gameId) {
 		return Mono.justOrEmpty(gameRepository.findById(gameId))
 				.switchIfEmpty(Mono.error(new IllegalArgumentException("Game ID: " + gameId + " not found.")));
+		
+	}
+	
+	@Transactional
+	public Mono<Game> nextPlayType(String gameId, String playType, int bid){
+		//PLACEHOLDER
+		Optional<Game> game = gameRepository.findById(gameId);
+		
+		return Mono.justOrEmpty(game)
+				.switchIfEmpty(Mono.error(new IllegalArgumentException("Game ID: " + gameId + " not found.")));
+		
+	}
+	
+	@Transactional
+	public Mono<Game> deleteGameById(String gameId){
+		
+		return null;
+		
+		
 		
 	}
 	
