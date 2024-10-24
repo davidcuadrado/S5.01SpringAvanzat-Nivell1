f0ca0bbeb872fbd4700fbbd2dfb5c43e9979562d
David Cuadrado <dav.cuadrado@gmail.com>
2024-10-24 05:25:32 +0200

    Swagger implementation and trim

--- S05T01N01/src/main/java/cat/itacademy/s05/t01/n01/swagger/SwaggerConfiguration.java
+++ S05T01N01/src/main/java/cat/itacademy/s05/t01/n01/swagger/SwaggerConfiguration.java
@@ -3,8 +3,6 @@ package cat.itacademy.s05.t01.n01.swagger;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class SwaggerConfiguration {
